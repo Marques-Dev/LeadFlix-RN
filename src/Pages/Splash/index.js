5c6c8cb81103982368eff49c0d081c03ce118224
Samuel Marques <samuelcabralmarques@gmail.com>
2021-07-21 19:22:49 -0300

    first commit

---
+++ src/Pages/Splash/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Splash = () => {
+    return (
+        <View>
+            <Text>Primeira tela emm</Text>
+        </View>
+    )
+}
+
+export default Splash
